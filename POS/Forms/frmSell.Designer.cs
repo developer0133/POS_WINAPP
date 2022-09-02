987c49794fc977e91d3f49934719a36d5914dde5
developer0133 <akkaradech12@gmail.com>
2022-09-02 16:46:24 +0700

    update

--- POS/Forms/frmSell.Designer.cs
+++ POS/Forms/frmSell.Designer.cs
@@ -30,20 +30,57 @@ namespace POS.Forms
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSell));
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label8.Location = new System.Drawing.Point(12, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 25);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "ขาย";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(17, 75);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(61, 17);
+            this.lblName.TabIndex = 42;
+            this.lblName.Text = "Barcode";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(95, 75);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(186, 22);
+            this.txtBarcode.TabIndex = 43;
             // 
             // frmSell
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 791);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label8);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmSell";
             this.Text = "Product System Service";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBarcode;
     }
 }
