99c2e44a890315975923295233c899bc7c3eb0b2
developer0133 <akkaradech12@gmail.com>
2022-12-25 12:27:51 +0700

    update

--- DAL/OrderHistory.cs
+++ DAL/OrderHistory.cs
@@ -61,63 +61,66 @@ namespace DAL
                     PRODUCTS = tmpProduct.Where(w => tmpCodeNew.Contains(w.PRODUCT_CODE)).ToList();
                 }
 
-                var qrydata = (from t in ORDER_HISTORY//_db.ORDER_HISTORY.Where(w => w.PRODUCT_ID == id)
-                               join t2 in PRODUCTS on t.PRODUCT_ID equals t2.PRODUCT_ID //_db.PRODUCTS on t.PRODUCT_ID equals t2.PRODUCT_ID
-                               join t3 in _db.PARAMETER.Where(w => w.MAJOR_CODE == POSPARAMETER.UNIT && w.STATUS == STATUS.ACTIVE) on t2.UNIT equals t3.MINOR_CODE into c1
-                               from t4 in c1.DefaultIfEmpty()
-                               select new
-                               {
-                                   HIST_ID = t.HIST_ID,
-                                   PRODUCT_ID = t.PRODUCT_ID.Value,
-                                   QTY = t.QTY,
-                                   UNIT = t.UNIT,
-                                   REMARK = t.REMARK,
-                                   AMOUNT = t.AMOUNT,
-                                   TOTAL_AMOUNT = t.TOTAL_AMOUNT,
-                                   ORDER_DATE = t.ORDER_DATE,
-                                   C_BY = t.C_BY,
-                                   C_DATE = t.C_DATE,
-                                   E_BY = t.E_BY,
-                                   E_DATE = t.E_DATE,
-                                   STR_UNIT = t4.NAME,
-
-                                   RETAILPRICE = t2.RETAILPRICE,
-                                   WHOLESALEPRICE = t2.WHOLESALEPRICE,
-                                   AVGCOST = t2.AVGCOST,
-                                   WHOLESALEPROFIT = t2.WHOLESALEPROFIT,
-                                   RETAILPROFIT = t2.RETAILPROFIT,
-                                   AVG_ITEM = t2.AVG_ITEM,
-                                   AVG_PACK = t2.AVG_PACK,
-                                   PRODUCT_NAME = t2.PRODUCT_NAME
-
-                               }).AsQueryable();
-                oList = (List<OrderHistoryDTO2>)qrydata.AsEnumerable().Select((s, index) => new OrderHistoryDTO2
+                if (ORDER_HISTORY.Count > 0 && PRODUCTS.Count() > 0)
                 {
-                    //No = (index + 1),
-                    // HIST_ID = s.HIST_ID,
-                    //PRODUCT_ID = s.PRODUCT_ID,
-                    REMARK = s.REMARK,
-                    QTY = s.QTY,
-                    //UNIT = s.UNIT,
-                    //ORDER_DATE = s.ORDER_DATE,
-                    //C_BY = s.C_BY,
-                    //C_DATE = s.C_DATE,
-                    //AMOUNT = s.AMOUNT,
-                    //TOTAL_AMOUNT = s.TOTAL_AMOUNT,
-                    STR_AMOUNT = string.Format("{0} {1}", s.AMOUNT, ""),
-                    STR_TOTAL_AMOUNT = string.Format("{0} {1}", s.TOTAL_AMOUNT, ""),
-                    STR_ORDERDATE = s.ORDER_DATE.Value.ToString("dd/MM/yyyy", clsFunction.formatThai),
-                    STR_UNIT = s.STR_UNIT,
+                    var qrydata = (from t in ORDER_HISTORY//_db.ORDER_HISTORY.Where(w => w.PRODUCT_ID == id)
+                                   join t2 in PRODUCTS on t.PRODUCT_ID equals t2.PRODUCT_ID //_db.PRODUCTS on t.PRODUCT_ID equals t2.PRODUCT_ID
+                                   join t3 in _db.PARAMETER.Where(w => w.MAJOR_CODE == POSPARAMETER.UNIT && w.STATUS == STATUS.ACTIVE) on t2.UNIT equals t3.MINOR_CODE into c1
+                                   from t4 in c1.DefaultIfEmpty()
+                                   select new
+                                   {
+                                       HIST_ID = t.HIST_ID,
+                                       PRODUCT_ID = t.PRODUCT_ID.Value,
+                                       QTY = t.QTY,
+                                       UNIT = t.UNIT,
+                                       REMARK = t.REMARK,
+                                       AMOUNT = t.AMOUNT,
+                                       TOTAL_AMOUNT = t.TOTAL_AMOUNT,
+                                       ORDER_DATE = t.ORDER_DATE,
+                                       C_BY = t.C_BY,
+                                       C_DATE = t.C_DATE,
+                                       E_BY = t.E_BY,
+                                       E_DATE = t.E_DATE,
+                                       STR_UNIT = t4.NAME,
 
-                    //RETAILPRICE = s.RETAILPRICE,
-                    //WHOLESALEPRICE = s.WHOLESALEPRICE,
-                    //AVGCOST = s.AVGCOST,
-                    //WHOLESALEPROFIT = s.WHOLESALEPROFIT,
-                    //RETAILPROFIT = s.RETAILPROFIT,
-                    AVG_ITEM = s.AVG_ITEM,
-                    AVG_PACK = s.AVG_PACK,
-                    PRODUCT_NAME = s.PRODUCT_NAME
-                }).OrderBy(a => a.STR_ORDERDATE).ToList();
+                                       RETAILPRICE = t2.RETAILPRICE,
+                                       WHOLESALEPRICE = t2.WHOLESALEPRICE,
+                                       AVGCOST = t2.AVGCOST,
+                                       WHOLESALEPROFIT = t2.WHOLESALEPROFIT,
+                                       RETAILPROFIT = t2.RETAILPROFIT,
+                                       AVG_ITEM = t2.AVG_ITEM,
+                                       AVG_PACK = t2.AVG_PACK,
+                                       PRODUCT_NAME = t2.PRODUCT_NAME
+
+                                   }).AsQueryable();
+                    oList = (List<OrderHistoryDTO2>)qrydata.AsEnumerable().Select((s, index) => new OrderHistoryDTO2
+                    {
+                        //No = (index + 1),
+                        // HIST_ID = s.HIST_ID,
+                        //PRODUCT_ID = s.PRODUCT_ID,
+                        REMARK = s.REMARK,
+                        QTY = s.QTY,
+                        //UNIT = s.UNIT,
+                        //ORDER_DATE = s.ORDER_DATE,
+                        //C_BY = s.C_BY,
+                        //C_DATE = s.C_DATE,
+                        //AMOUNT = s.AMOUNT,
+                        //TOTAL_AMOUNT = s.TOTAL_AMOUNT,
+                        STR_AMOUNT = string.Format("{0} {1}", s.AMOUNT, ""),
+                        STR_TOTAL_AMOUNT = string.Format("{0} {1}", s.TOTAL_AMOUNT, ""),
+                        STR_ORDERDATE = s.ORDER_DATE.Value.ToString("dd/MM/yyyy", clsFunction.formatThai),
+                        STR_UNIT = s.STR_UNIT,
+
+                        //RETAILPRICE = s.RETAILPRICE,
+                        //WHOLESALEPRICE = s.WHOLESALEPRICE,
+                        //AVGCOST = s.AVGCOST,
+                        //WHOLESALEPROFIT = s.WHOLESALEPROFIT,
+                        //RETAILPROFIT = s.RETAILPROFIT,
+                        AVG_ITEM = s.AVG_ITEM,
+                        AVG_PACK = s.AVG_PACK,
+                        PRODUCT_NAME = s.PRODUCT_NAME
+                    }).OrderBy(a => a.STR_ORDERDATE).ToList();
+                }
 
                 return oList;
                 //return await _db.ORDER_HISTORY.Where(w => w.PRODUCT_ID == id).ToListAsync();
