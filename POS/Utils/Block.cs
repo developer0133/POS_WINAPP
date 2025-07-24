6e1a95c41725c4c360c552a67de8b075362fb298
developer0133 <akkaradech12@gmail.com>
2025-07-24 14:20:14 +0700

    update

---
+++ POS/Utils/Block.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Utils
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+
+        public Block(int index, DateTime timestamp, string data, string previousHash)
+        {
+            Index = index;
+            Timestamp = timestamp;
+            Data = data;
+            PreviousHash = previousHash;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string rawData = Index + Timestamp.ToString() + Data + PreviousHash;
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                string utfString = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                byte[] data = FromHex(utfString);
+                string s = Encoding.ASCII.GetString(data);
+                return Encoding.ASCII.GetString(data);
+            }
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+    }
+}
---
+++ POS/Utils/Blockchain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Utils
+{
+    public class Blockchain
+    {
+        public List<Block> Chain { get; set; }
+
+        public Blockchain()
+        {
+            Chain = new List<Block> { CreateGenesisBlock() };
+        }
+
+        private Block CreateGenesisBlock()
+        {
+            return new Block(0, DateTime.UtcNow, "Genesis Block", "0");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(string data)
+        {
+            Block latest = GetLatestBlock();
+            Block newBlock = new Block(latest.Index + 1, DateTime.UtcNow, data, latest.Hash);
+            Chain.Add(newBlock);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block current = Chain[i];
+                Block previous = Chain[i - 1];
+
+                if (current.Hash != current.CalculateHash()) return false;
+                if (current.PreviousHash != previous.Hash) return false;
+            }
+            return true;
+        }
+    }
+}
